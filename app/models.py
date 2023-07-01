b2e5c69b3e9dfef82cfc4f3d478e1dad0a014cc6
Wei Xia <xiawei27149@gmail.com>
2023-06-30 21:59:13 -0700

    dynamic notification

--- app/models.py
+++ app/models.py
@@ -2,6 +2,8 @@ from app import db, login
 from flask import current_app
 from time import time
 import jwt
+import json
+from time import time
 from datetime import datetime
 from flask_login import UserMixin
 from hashlib import md5
@@ -32,6 +34,8 @@ class User(UserMixin, db.Model):
                                         foreign_keys='Message.recipient_id',
                                         backref='recipient', lazy='dynamic')
     last_message_read_time = db.Column(db.DateTime)
+    notifications = db.relationship('Notification', backref='user',
+                                    lazy='dynamic')
 
     followed = db.relationship(
         'User', secondary=followers,
@@ -92,6 +96,12 @@ class User(UserMixin, db.Model):
         last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
         return Message.query.filter_by(recipient=self).filter(
             Message.timestamp > last_read_time).count()
+    
+    def add_notification(self, name, data):
+        self.notifications.filter_by(name=name).delete()
+        n = Notification(name=name, payload_json=json.dumps(data), user=self)
+        db.session.add(n)
+        return n
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -116,3 +126,14 @@ class Message(db.Model):
 
     def __repr__(self):
         return '<Message {}>'.format(self.body)
+    
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.Float, index=True, default=time)
+    payload_json = db.Column(db.Text)
+
+    def get_data(self):
+        return json.loads(str(self.payload_json))
